dc6eb843a21f143caed984769e71ea08a75b092c
Veronica Bustabad <vbustabad@gmail.com>
2018-04-10 03:34:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,14 +8,6 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def artist=(artist)
     @artist = artist
   end
