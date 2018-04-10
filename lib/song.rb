6a7bbfe35871e181857e6d716430f25701ddd927
Veronica Bustabad <vbustabad@gmail.com>
2018-04-10 03:35:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,9 @@ class Song
   attr_accessor :name
   attr_reader :artist
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@songs = []
 
